05187c015f299bb80702ba11d6ffb46740f8ec6b
Salmankhan0518 <salmansharif0518@gmail.com>
2026-03-27 20:28:19 +0500

    add css and new input fields

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,19 +1,42 @@
-import React from "react"
+import React from "react";
 import "./Auth.css";
 
 function Register() {
+  return (
+    <div className="auth-container">
+      <form className="auth-form">
+        <h2 className="auth-title">Create an Account</h2>
 
-    return(
-        <div className="auth-container">
-        <form className="auth-form">
-            <h2 className="auth-title">Create an Account</h2>
-            <div className="input-group">
-                <label htmlFor="name" className="input-label">Full Name</label>
-                <input id="name" type="text" className="input-text" />
-            </div>
-        </form>
+        <div className="input-group">
+          <label htmlFor="name" className="input-label">
+            Full Name
+          </label>
+          <input id="name" type="text" className="input-text" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="email" className="input-label">
+            Email
+          </label>
+          <input id="email" type="text" className="input-text" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="mobile" className="input-label">
+            Mobile
+          </label>
+          <input id="mobile" type="text" className="input-text" />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password" className="input-label">
+            Password
+          </label>
+          <input id="password" type="text" className="input-text" />
+        </div>
+      </form>
     </div>
-    )
+  );
 }
 
 export default Register;
